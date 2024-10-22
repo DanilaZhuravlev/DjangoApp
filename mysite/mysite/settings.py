e3690340e8b74f71bbc8f41d124d3bde4ab2ee88
Danila <d.zhuravlev94@gmail.com>
2024-10-22 10:47:32 +0300

    configure for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+import logging.config
 
 import django_filters
 from django.urls import reverse_lazy
@@ -20,19 +22,23 @@ import myapiapp
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATABASE_DIR = BASE_DIR / 'database'
+DATABASE_DIR.mkdir(exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s(+_wwjpnfn9@i$^b4abec6^j(-i3^(9jnj&j5i0xea)pyr)56'
+SECRET_KEY = getenv(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-s(+_wwjpnfn9@i$^b4abec6^j(-i3^(9jnj&j5i0xea)pyr)56',
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
-INTERNAL_IPS = ['127.0.0.1']
+INTERNAL_IPS = ['127.0.0.1'] + getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 if DEBUG:
     import socket
@@ -109,7 +115,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -212,3 +218,29 @@ LOGGING = {
         'level': 'INFO',
     }
 }
+
+LOGLEVEL = getenv('DJANGO_LOGLEVEL', 'INFO')
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console':{
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s',
+        },
+    },
+    'handlers':{
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers':{
+        '':{
+            'level': LOGLEVEL,
+            'handlers': [
+                'console'
+            ],
+        },
+    },
+})
